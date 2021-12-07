fd843bac3cf174825bd6ecf11232555ef502a452
Alexey Kulichenko <alcul265@gmail.com>
2021-12-07 19:33:13 +0600

    connecting to file

---
+++ ui_file.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.create_circle = QtWidgets.QPushButton(self.centralwidget)
+        self.create_circle.setGeometry(QtCore.QRect(290, 440, 201, 81))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.create_circle.setFont(font)
+        self.create_circle.setObjectName("create_circle")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.create_circle.setText(_translate("MainWindow", "Create circle"))
